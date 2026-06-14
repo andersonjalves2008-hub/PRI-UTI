101ea4f7d2ad3163aa2e600e29bd08a5bb0e2527
andersonjalves2008-hub <andersonjalves2008@gmail.com>
2026-06-14 20:34:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ if "modelo_usado" not in st.session_state:
 
 st.title("🏥 PRI-UTI")
 st.subheader("Sistema Inteligente de Priorização para Admissão em UTI")
-st.caption("PRI-UTI v1.0 • Desenvolvido por Anderson José Alves")
+st.caption("PRI-UTI v1.0 • Desenvolvido por Anderson José Alves - Qualimed")
 st.divider()
 
 caso = st.text_area(
